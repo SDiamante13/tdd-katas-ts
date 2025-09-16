74353da63b4364d9bdfa50ec03856e49a528d04f
Steven Diamante <stevendiamante13@gmail.com>
2025-09-16 10:17:35 -0400

    Add new tests that use external api to get random quote.

--- katas/greeting-service/tests/acceptance/greetings.acceptance.test.ts
+++ katas/greeting-service/tests/acceptance/greetings.acceptance.test.ts
@@ -1,11 +1,27 @@
 import request from 'supertest';
 import app from "../../src";
 import { clearAllTables, createTestUser, getLatestGreeting } from '../setup/testUtils';
+import { setupServer } from "msw/node";
+import { http, HttpResponse } from "msw";
 
 describe('Greetings with favorite quotes', () => {
+
+    const server = setupServer(
+        http.get('https://fakerapi.it/api/v2/texts*', () => {
+            return HttpResponse.json({
+                data: [{
+                    content: "Make today amazing!"
+                }]
+            });
+        }));
+
     beforeEach(async () => {
         await clearAllTables();
     });
+
+    beforeAll(() => server.listen());
+    afterEach(() => server.resetHandlers());
+    afterAll(() => server.close());
 
     test('user gets personalized greeting without favorite quote when include_quotes is false', async () => {
         const includeQuotes = false;
@@ -20,10 +36,18 @@ describe('Greetings with favorite quotes', () => {
             {message: 'What\'s up Test User!'},
             {message: 'Hey Test User!'}
         ]);
-
         expect(await getLatestGreeting(userId)).toMatchObject({
             user_id: userId,
             greeting_type: 'time_based'
+        });
+    });
+
+    test('user should get random quote', async () => {
+        const response = await whenARequestIsSendTo("/api/greeting?withQuote=true&name=Rob");
+
+        expect(response.status).toBe(200);
+        expect(response.body).toMatchObject({
+            message: "Hello Rob! Here's something to inspire your day: 'Make today amazing!'"
         });
     });
 });
